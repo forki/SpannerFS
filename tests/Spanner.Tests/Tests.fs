2bbedc07203f7570f7bc7d2db97fb5f47d80b448
jmjrawlings <justin.m.rawlings@gmail.com>
2017-01-29 09:35:01 +1000

    remove string case

--- tests/Spanner.Tests/Tests.fs
+++ tests/Spanner.Tests/Tests.fs
@@ -7,7 +7,7 @@ open NUnit.Framework
 [<Test>]
 let ``empty unions are identity`` () =
 
-  let a = Span.ofPair false (1, 100)
+  let a = Span.ofBounds false (1, 100)
   let b = Span.Empty
   let c = Span.union a b
     
@@ -17,18 +17,17 @@ let ``empty unions are identity`` () =
 [<Test>]
 let ``validating unordered pairs is empty`` () =
 
-    let s = Span.ofPair true (10, -10)
+    let s = Span.ofBounds true (10, -10)
 
     Assert.IsTrue( s |> Span.isEmpty )
 
 
 [<TestCase(-100, 20)>]
 [<TestCase(-0.1f, 2.0f)>]
-[<TestCase("a", "f")>]
 [<TestCase(-1023L, 20000L)>]
 let ``ignoring validation is symetrical`` (a: 'T, b: 'T) =
 
-    let createNoValidate = Span.ofPair false
+    let createNoValidate = Span.ofBounds false
 
     let forward = createNoValidate (a, b)
     let backward = createNoValidate (b, a)
